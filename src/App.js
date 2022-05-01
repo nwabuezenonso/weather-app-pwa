820231ae9b01246478ecb7bce40684a05494ca2c
stephen <nwabuezesc@gmail.com>
2022-05-01 14:14:06 +0100

    passing in weather API and function to access API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
+
 
 function App() {
+
+  const [ query, setQuery] = useState('');
+
+  const search = async (e) =>{
+    if( e.key === 'Enter'){
+      const data =  await fetchWeather(query)
+
+      console.log(data)
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="main-container">
+      <input 
+        type="text"
+        className='search'
+        placeholder='Search...'
+        value={query}
+        onChange={ (e) => setQuery(e.target.value) }
+        onKeyPress= {search}
+      />
     </div>
   );
 }
