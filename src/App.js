3a67690c5105567146b3101dca06028464d92291
stephen <nwabuezesc@gmail.com>
2022-05-01 14:37:28 +0100

    applying comments and setting query to be an empty string

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import { fetchWeather } from './api/fetchWeather';
 
 
 function App() {
-
+  //setting state for query and weather
   const [ query, setQuery] = useState('');
+  const [ weather, setWeather] = useState({});
 
+  //function that will receive data from the featherweather api
   const search = async (e) =>{
     if( e.key === 'Enter'){
-      const data =  await fetchWeather(query)
+      const data =  await fetchWeather(query);
 
-      console.log(data)
+      //set weather to be the data
+      setWeather(data);
+
+      //setquery to be empty
+      setQuery('')
     }
   }
 
+  //returning a set of jsx and passing the state on the onclick function
+  //call the function on the onkeypress function
   return (
     <div className="main-container">
       <input 
@@ -28,4 +36,5 @@ function App() {
   );
 }
 
+//exporting app
 export default App;
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+//variables for url and apikey
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
+//creating a function to get data from another service
 export const fetchWeather = async ( query ) => {
     const { data } = await axios.get(URL, {
+        //query params  is a way to pass information to the url
         params: {
             q: query,
             units: 'metric',
@@ -12,5 +15,6 @@ export const fetchWeather = async ( query ) => {
         }
     });
 
+    //return data
     return data;
 }
